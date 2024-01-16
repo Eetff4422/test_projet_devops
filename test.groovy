20d6e16a90e6a412407dfdfce48d18399329d420
Eetff4422 <116736976+Eetff4422@users.noreply.github.com>
2024-01-16 21:11:23 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -19,116 +19,127 @@ pipeline {
                 }
             }
         }
-        // stage('Fevrier'){
-        //     when {
-        //     expression { params.MOIS == 'Fevrier' } 
-        //     }
-        //     steps{
-        //         script{
-                
-        //         }
-        //     }
-        // }
-        // stage('Mars'){
-        //     when {
-        //     expression { params.MOIS == 'Mars' } 
-        //     }
-        //     steps{
-        //         script{
-    
-        //         }
-        //     }
-        // }
-        // stage('Avril'){
-        //     when {
-        //     expression { params.MOIS == 'Avril' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Mai'){
-        //     when {
-        //     expression { params.MOIS == 'Mai' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Juin'){
-        //     when {
-        //     expression { params.MOIS == 'Juin' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Juillet'){
-        //     when {
-        //     expression { params.MOIS == 'Juillet' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Aout'){
-        //     when {
-        //     expression { params.MOIS == 'Aout' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Septembre'){
-        //     when {
-        //     expression { params.MOIS == 'Septembre' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Octobre'){
-        //     when {
-        //     expression { params.MOIS == 'Octobre' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Novembre'){
-        //     when {
-        //     expression { params.MOIS == 'Novembre' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
-        // stage('Decembre'){
-        //     when {
-        //     expression { params.MOIS == 'Decembre' } 
-        //     }
-        //     steps{
-        //         script{
-                    
-        //         }
-        //     }
-        // }
+        stage('Fevrier'){
+            when {
+            expression { params.MOIS == 'Fevrier' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Fevrier.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Mars'){
+            when {
+            expression { params.MOIS == 'Mars' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Mars.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Avril'){
+            when {
+            expression { params.MOIS == 'Avril' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Avril.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Mai'){
+            when {
+            expression { params.MOIS == 'Mai' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Mai.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Juin'){
+            when {
+            expression { params.MOIS == 'Juin' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Juin.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Juillet'){
+            when {
+            expression { params.MOIS == 'Juillet' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Juillet.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Aout'){
+            when {
+            expression { params.MOIS == 'Aout' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Aout.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Septembre'){
+            when {
+            expression { params.MOIS == 'Septembre' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Septembre.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Octobre'){
+            when {
+            expression { params.MOIS == 'Octobre' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Octobre.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
+        stage('Novembre'){
+            when {
+            expression { params.MOIS == 'Novembre' } 
+            }
+            steps{
+                script{
+                     def jenkinsfilePath = "Novembre.groovy"
+                    load jenkinsfilePath   
+                }
+            }
+        }
+        stage('Decembre'){
+            when {
+            expression { params.MOIS == 'Decembre' } 
+            }
+            steps{
+                script{
+                    def jenkinsfilePath = "Decembre.groovy"
+                    load jenkinsfilePath
+                }
+            }
+        }
     }
 }
 
