f5ea677b0e0e38260ba4e5bff4badfea2d1dc773
Eetff4422 <frangiessono1@gmail.com>
2024-01-16 16:42:25 +0100

    Ajout des fichiers

---
+++ Janvier.groovy
@@ -0,0 +1,74 @@
+def genererEntreeJournal(nom) {
+    return [titre: "Journal de $nom", texte: "Texte de l'entrée du journal"]
+}
+
+def enregistrerDonnees(donnees) {
+    println("Données enregistrées en base de données : $donnees")
+}
+def trackerActivites(objectifs) {
+    return [sport: 1, meditation: 7] 
+}
+def envoyerNotification(message) {
+    println("Notification envoyée : $message")
+}
+
+def moisJanvier(params, objectifs, activites, progression) {
+    // Initialiser le suivi
+    def objectifs = [
+        "ecrireJournal" : 31, // nb jours objectif
+        "lireLivre": 1  // nb livres objectif
+    ]
+
+    def activites = trackerActivites(objectifs)
+
+    def progression = [
+        "ecrireJournal": 0,
+        "lireLivre": 0
+    ]
+    def progression_after_15 = [
+        "ecrireJournal" : 15,
+        "lireLivre" : 0
+    ]
+
+    // Calculer progression en fonction du jour
+    def jourActuel = params.JOUR.toInteger()
+
+    if (jourActuel >= 1 && jourActuel < 15) {
+        progression.ecrireJournal = jourActuel
+        genererEntreeJournal("${params.NOM}").with {
+            enregistrerDonnees(it)
+            envoyerNotification("Nouvelle entrée pour ${params.NOM}")
+        }
+    } else {
+        if (progression_after_15.ecrireJournal >= 15 && jourActuel == progression_after_15.ecrireJournal) {
+            if (progression_after_15.lireLivre == 1) {
+                println "Tu es sur la bonne voie, continue de remplir quotidiennement ton journal et tu atteindras ton objectif du mois."
+            } else {
+                println "Tu es sur la bonne voie mais n'oublie pas de lire $objectifs.lireLivre livre pour atteindre tous tes objectifs."
+            }
+
+        } else {
+            if (jourActuel >= 15) {
+                if (progression_after_15.ecrireJournal == 15) {
+                    objectifs.ecrireJournal = 20
+                } else {
+                    objectifs.ecrireJournal = 15
+                }
+            } else {
+                progression.ecrireJournal = 5
+            }
+
+            objectifs.lireLivre = 1
+
+            println "Malheureusement je vois que t'auras du mal à réaliser tes objectifs. Mais ne t'inquiète pas je les ai réajustés pour te permettre de les atteindre. Bonne chance"
+        }
+    }
+
+    // Afficher progression
+    println "Objectifs à atteindre: $objectifs"
+    if (jourActuel < 15) {
+        println "Progression au $jourActuel ${params.MOIS} : $progression"
+    } else {
+        println "Progression au $jourActuel ${params.MOIS} : $progression_after_15"
+    }
+}
---
+++ test.groovy
@@ -0,0 +1,139 @@
+@Grapes([
+  @Grab(group='monrepo', module='src', version='master', classifier='Fichier1') 
+])
+
+import monrepo.src.Fichier1
+
+pipeline {
+    agent any
+
+    parameters {
+        choice(name: 'MOIS', choices: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet','Aout', 'Septembre', 'Octobre','Novembre', 'Decembre'], description: 'Choisissez le mois')
+        string(name: 'NOM', defaultValue: 'Jean', description: 'Entrez votre nom')
+        choice(name: 'JOUR', choices: ['1', '2', '3', '4', '5', '6', '7','8', '9', '10','11', '12','13', '14', '15', '16', '17', '18', '19','20', '21', '22','23', '24','25','26','27','28','29','30','31'], description: 'Quel est la date du jour ?')
+    }
+
+    stages {
+        stage('Janvier') {
+            when {
+            expression { params.MOIS == 'Janvier' } 
+            }
+            steps {
+                script {
+                    
+                }
+            }
+        }
+        stage('Fevrier'){
+            when {
+            expression { params.MOIS == 'Fevrier' } 
+            }
+            steps{
+                script{
+                
+                }
+            }
+        }
+        stage('Mars'){
+            when {
+            expression { params.MOIS == 'Mars' } 
+            }
+            steps{
+                script{
+    
+                }
+            }
+        }
+        stage('Avril'){
+            when {
+            expression { params.MOIS == 'Avril' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Mai'){
+            when {
+            expression { params.MOIS == 'Mai' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Juin'){
+            when {
+            expression { params.MOIS == 'Juin' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Juillet'){
+            when {
+            expression { params.MOIS == 'Juillet' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Aout'){
+            when {
+            expression { params.MOIS == 'Aout' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Septembre'){
+            when {
+            expression { params.MOIS == 'Septembre' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Octobre'){
+            when {
+            expression { params.MOIS == 'Octobre' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Novembre'){
+            when {
+            expression { params.MOIS == 'Novembre' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+        stage('Decembre'){
+            when {
+            expression { params.MOIS == 'Decembre' } 
+            }
+            steps{
+                script{
+                    
+                }
+            }
+        }
+    }
+}
+
